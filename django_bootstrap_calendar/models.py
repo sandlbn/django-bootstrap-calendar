cf025969d7cbf2d78cfbf890967fd6a67fbc53c6
Manuel Kaufmann <humitos@gmail.com>
2014-05-27 11:01:18 -0300

    Use ugettext_lazy instead of ugettext to make it compatible with Django 1.7
    https://docs.djangoproject.com/en/dev/ref/applications/#applications-troubleshooting

--- django_bootstrap_calendar/models.py
+++ django_bootstrap_calendar/models.py
@@ -2,7 +2,7 @@
 __author__ = 'sandlbn'
 
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from utils import datetime_to_timestamp
 
 
